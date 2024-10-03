d2ce063a01eff2d64181d85377c84c4931b75661
Carter H <chend85@lsu.edu>
2024-10-02 23:49:58 -0500

    update

---
+++ options1.py
@@ -0,0 +1,33 @@
+from DLProjectPySide import YouTubeDownloaderFrame
+def create_opts_list(self):
+    download_dir1 = str
+    self.ydl_opts_list = [
+
+            # First version of ydl_opts
+        {
+            'format': 'mp3/bestaudio/best',
+            'paths': {'home': download_dir1},
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'mp3',
+            }],
+        },
+        {
+            'format': 'bestvideo+bestaudio',
+            'paths': {'home': download_dir1},
+            'postprocessors': [{
+                'key': 'FFmpegVideoConvertor',
+                'preferedformat': 'mp4',
+            }],
+
+        },
+        {
+            'format': 'bestaudio',
+            'paths': {'home': download_dir1},
+            'postprocessors': [{
+                'key': 'FFmpegExtractAudio',
+                'preferredcodec': 'wav',
+            }],
+        }
+    ]
+    return self.ydl_opts_list
